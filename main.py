ab373d4b9173539d34e786c37c80a867e8154caf
MeNæm <108131422+MeNaemS@users.noreply.github.com>
2023-06-25 16:14:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
 from checker.checker import Checker
-from selenium import webdriver
+from ChromeDriver.Chrome import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
-
-
-class Chrome(webdriver.Chrome):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.set_window_size(950, 700)
-        self.set_window_position(250, 50)
-
-    def click_element(self, by: By, element: str, timeout: float = 10):
-        WebDriverWait(self, timeout=timeout).until(ec.element_to_be_clickable((by, element)))
-        self.find_element(by, element).click()
-
-    def send_keys(self, by: By, element: str, value: str, timeout: int = 10):
-        WebDriverWait(self, timeout=timeout).until(ec.visibility_of_element_located((by, element)))
-        self.find_element(by, element).send_keys(value)
 
 
 def yandex_backup(value: str, password: str):
-    options = webdriver.ChromeOptions()
+    options = ChromeOptions()
     # options.add_argument("--headless=new")
     options.add_experimental_option("detach", True)
-    options.add_experimental_option('excludeSwitches', ['enable-automation'])
-    options.add_experimental_option('useAutomationExtension', False)
     driver = Chrome(options=options)
     driver.get('https://360.yandex.com/disk/')
     driver.click_element(By.ID, 'header-login-button')
@@ -36,9 +17,9 @@ def yandex_backup(value: str, password: str):
 
 
 if __name__ == '__main__':
-    value = 'ilikesleeping268@ya.ru'
+    value = input()
     check_adress = Checker(value)
     if check_adress.boolean is False:
         raise ValueError('Email address is not valid')
-    password = 'GooglePasswordMadeWithMe100%'
+    password = input()
     yandex_backup(value, password)
