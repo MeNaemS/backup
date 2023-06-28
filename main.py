06e1962ac83d203136e42080981af7f4ab42ce22
MeNæm <108131422+MeNaemS@users.noreply.github.com>
2023-06-28 18:32:53 +0300

    Update main.py
    Reducing the amount of code in the main.py file (transferring part of the code to the disks/yandex_disk file)

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
 from checker.checker import Checker
-from ChromeDriver.Chrome import Chrome, ChromeOptions
-from selenium.webdriver.common.by import By
+from disks.yandex_disk import yandex_backup
 
 
 def choose_file() -> str:
@@ -12,39 +10,8 @@ def choose_file() -> str:
     return QFileDialog.getOpenFileNames(caption='Choose files')[0]
 
 
-def yandex_backup(value: str, password: str, option: str):
-    options = ChromeOptions()
-    # options.add_argument("--headless=new")
-    options.add_experimental_option("detach", True)
-    driver = Chrome(options=options)
-    driver.get('https://360.yandex.com/disk/')
-    driver.click_element(By.ID, 'header-login-button')
-    option_button = [
-        driver.find_element(By.CSS_SELECTOR, 'button[data-type="login"]'),
-        driver.find_element(By.CSS_SELECTOR, 'button[data-type="phone"]')
-    ]
-    if (option == 'email') and (option_button[0].get_attribute('aria-pressed') == 'false'):
-        option_button[0].click()
-    if (option == 'phone') and (option_button[1].get_attribute('aria-pressed') == 'false'):
-        option_button[1].click()
-    time.sleep(0.5)
-    driver.send_keys(By.CSS_SELECTOR, 'input[type=text]', value)
-    driver.click_element(By.ID, 'passp:sign-in')
-    time.sleep(0.5)
-    login_url = 'https://passport.yandex.com/auth?retpath=https%3A%2F%2Fdisk.yandex.com'
-    if driver.current_url == login_url:
-        raise ValueError("Account doesn't exist")
-    driver.send_keys(By.CSS_SELECTOR, 'input[type=password]', password)
-    driver.click_element(By.ID, 'passp:sign-in')
-    time.sleep(1)
-    password_url = 'https://passport.yandex.com/auth/welcome?retpath=https%3A%2F%2Fdisk.yandex.com'
-    if driver.current_url == password_url:
-        raise ValueError("Incorrect password")
-
-
 if __name__ == '__main__':
     file_path = choose_file()
-    print(file_path)
     value = 'ilikesleeping268@ya.ru'
     check_adress = Checker(value)
     if check_adress.boolean is False:
@@ -55,5 +22,4 @@ if __name__ == '__main__':
         print('We apologize, at the moment support for a phone number for backup in Yandex Disk is not available')
         exit()
     password = 'GooglePasswordMadeWithMe'
-    yandex_backup(value, password, check_adress.option)
-
+    yandex_backup(value, password, check_adress.option, file_path)
